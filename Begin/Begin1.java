3a2f317c1c2b4ac735137561772ad3ebe47be61f
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2022-05-14 15:19:48 +0500

    test

---
+++ Begin/Begin1.java
@@ -0,0 +1,9 @@
+package Begin;
+
+public class Begin1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub");
+    }
+
+}
---
+++ Begin/Begin2.java
@@ -0,0 +1,5 @@
+package Begin;
+
+public class Begin2 {
+
+}
