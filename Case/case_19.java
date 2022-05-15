54d779eccb9e04359de32e4e61716af831521c05
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2022-05-15 18:32:32 +0500

    package name fixed

--- Case/case_19.java
+++ Case/case_19.java
@@ -1,4 +1,4 @@
-package gita.foundation.problems_5;
+package Case;
 
 import java.util.Scanner;
 
@@ -14,18 +14,18 @@ public class case_19 {
         color = (year - 1984) % 5;
 
         switch (animal) {
-            case 0  -> System.out.print(year + " - sichqon");
-            case 1  -> System.out.print(year + " - sigir");
-            case 2  -> System.out.print(year + " - yo'lbars");
-            case 3  -> System.out.print(year + " - quyon");
-            case 4  -> System.out.print(year + " - ajdar");
-            case 5  -> System.out.print(year + " - ilon");
-            case 6  -> System.out.print(year + " - ot");
-            case 7  -> System.out.print(year + " - qo'y");
-            case 8  -> System.out.print(year + " - maymun");
-            case 9  -> System.out.print(year + " - tovuq");
-            case 10  -> System.out.print(year + " - it");
-            case 11  -> System.out.print(year + " - to'ng'iz");
+            case 0 -> System.out.print(year + " - sichqon");
+            case 1 -> System.out.print(year + " - sigir");
+            case 2 -> System.out.print(year + " - yo'lbars");
+            case 3 -> System.out.print(year + " - quyon");
+            case 4 -> System.out.print(year + " - ajdar");
+            case 5 -> System.out.print(year + " - ilon");
+            case 6 -> System.out.print(year + " - ot");
+            case 7 -> System.out.print(year + " - qo'y");
+            case 8 -> System.out.print(year + " - maymun");
+            case 9 -> System.out.print(year + " - tovuq");
+            case 10 -> System.out.print(year + " - it");
+            case 11 -> System.out.print(year + " - to'ng'iz");
 
         }
 
