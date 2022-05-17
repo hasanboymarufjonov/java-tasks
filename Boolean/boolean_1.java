26e5105ee79bf8bffa4561cbc434caf8e5b53708
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2022-05-17 20:26:33 +0500

    taking input fixed

--- Boolean/boolean_1.java
+++ Boolean/boolean_1.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class boolean_1 {
     public static void main(String[] args) {
         int a;
-        Scanner input = new Scanner(System.in);
-        a = input.nextInt();
-
+        try (Scanner input = new Scanner(System.in)) {
+            a = input.nextInt();
+        }
         boolean result = a > 0;
 
         System.out.println(result);
