2f6f4ed96d8431feffd0d2c9988cc329b2f30b11
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2022-05-19 21:45:06 +0500

    file name changed

--- If/if_27.java
+++ If/if_27.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class if_27 {
     public static void main(String[] args) {
         double x;
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("X = ");
+            x = input.nextInt();
+        }
 
-        System.out.print("X = ");
-        x = input.nextInt();
+        int integer = (int)x;
 
         if (x < 0) {
             x = 0;
-        } else if (x % 2 == 0) {
+        } else if (integer % 2 == 0) {
             x = 1;
-        } else if (x % 2 == 1) {
+        } else if (integer % 2 == 1) {
             x = -1;
         }
 
