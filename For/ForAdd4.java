95061270decfabb0022a8a6cb9c8a8f797937e6e
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2022-05-28 20:55:52 +0500

    upload file

---
+++ For/ForAdd4.java
@@ -0,0 +1,11 @@
+package For;
+
+// N natural soni berilgan shu songacha berilgan mukammal sonlarni chiqaruvchi dastur tuzilsin
+
+public class ForAdd4 {
+    private static void input(int N) {
+        for (int i = 1; i < N; i++) {
+
+        }
+    }
+}
