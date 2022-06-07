87ae777cf2547e0e5891bb1c5c3ed04ec8dfe7a6
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2022-06-07 17:25:14 +0500

    upload files

--- While/While4.java
+++ While/While4.java
@@ -2,20 +2,21 @@ package While;
 
 public class While4 {
     public static void main(String[] args) {
-        int n = 27;
+        int n = 9;
         int three = 3;
 
         // 3, 9, 27, 81
 
-        while (three <= n) {
+        while (three < n) {
             three *= 3;
-            if (three == n) {
-                System.out.println("true");
-                break;
-            } else {
-                System.out.println("false");
-                break;
-            }
+
+        }
+        if (three == n) {
+            System.out.println("true");
+
+        } else {
+            System.out.println("false");
+
         }
 
     }
--- While/While6.java
+++ While/While6.java
@@ -1,7 +1,14 @@
 package While;
 
+import javax.swing.plaf.TreeUI;
+
 public class While6 {
     public static void main(String[] args) {
-
+        int n = 67;
+        while (n > 0) {
+            n *= n;
+            n -= 2;
+        }
+        System.out.println(n);
     }
 }
